f55c2379fcdf6b7b204f6fb763a94229e3474f71
Arun Giri <AGiri09@my.fisk.edu>
2023-10-30 13:46:47 -0500

    new changes

--- Trie/DesignAddandsearchwordsDS.py
+++ Trie/DesignAddandsearchwordsDS.py
@@ -1,4 +1,3 @@
-
 class TrieNode:
     def __init__(self):
         self.children = {}
@@ -18,15 +17,22 @@ class WordDictionary:
         current_node.endofword = True
 
     def search(self, word: str) -> bool:
-        current_node = self.root
-        for char in word:
-            if char == ".":
-               current_node = current_node.children[char]
-            if char not in current_node.children:
-                return False
-            current_node = current_node.children[char]
-        return current_node.endofword 
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.endofword
 
+        return dfs(0, self.root)
 
 
 # Your WordDictionary object will be instantiated and called as such:
